65eb84eb534bbd4e6a56ec96cc6f14fee0ef13fc
chattarinpardermwong <43002685+chattarinpardermwong@users.noreply.github.com>
2019-02-11 11:40:58 +0700

    Update myscript.js

--- jquery3/myscript.js
+++ jquery3/myscript.js
@@ -25,8 +25,7 @@ $(document).ready(function() {
 
 $('#search-btn').click(function() {
     $('#table').text('')
-    let val = $(this).val()
-    val = val.toLowerCase()
+    var word = $('#input-text').val().toLowerCase();
     $.ajax({
         url: "data.json",
         dataType: "json"
@@ -34,8 +33,7 @@ $('#search-btn').click(function() {
         console.log(response);
         var x = "";
         response.forEach(element => {
-            if(element.id.includes($('#input-text').val())){
-                console.log(element.id,element.name)
+            if(element.id.toLowerCase().includes(word)||element.gender.toLowerCase().includes(word)||element.class.toLowerCase().includes(word)){
                 x +="<tr>"+
                 "<td>"+element.id+"</td>"+
                 "<td>"+element.name+"</td>"+
@@ -49,53 +47,7 @@ $('#search-btn').click(function() {
                 "</tr>";
                 $('#datatable').html(x);
             }
-            else if(element.name.includes($('#input-text').val())){
-                console.log(element.id,element.name)
-                x +="<tr>"+
-                "<td>"+element.id+"</td>"+
-                "<td>"+element.name+"</td>"+
-                "<td>"+element.gender+"</td>"+
-                "<td>"+element.age+"</td>"+
-                "<td>"+element.class+"</td>"+
-                "<td>"+element.Atk+"</td>"+
-                "<td>"+element.MAtk+"</td>"+
-                "<td>"+element.Def+"</td>"+
-                "<td>"+element.SPD+"</td>"+
-                "</tr>";
-                $('#datatable').html(x);
-            }
-            else if(element.gender.includes($('#input-text').val())){
-                console.log(element.id,element.name)
-                x +="<tr>"+
-                "<td>"+element.id+"</td>"+
-                "<td>"+element.name+"</td>"+
-                "<td>"+element.gender+"</td>"+
-                "<td>"+element.age+"</td>"+
-                "<td>"+element.class+"</td>"+
-                "<td>"+element.Atk+"</td>"+
-                "<td>"+element.MAtk+"</td>"+
-                "<td>"+element.Def+"</td>"+
-                "<td>"+element.SPD+"</td>"+
-                "</tr>";
-                $('#datatable').html(x);
-            }
-            else if(element.age.includes($('#input-text').val())){
-                console.log(element.id,element.name)
-                x +="<tr>"+
-                "<td>"+element.id+"</td>"+
-                "<td>"+element.name+"</td>"+
-                "<td>"+element.gender+"</td>"+
-                "<td>"+element.age+"</td>"+
-                "<td>"+element.class+"</td>"+
-                "<td>"+element.Atk+"</td>"+
-                "<td>"+element.MAtk+"</td>"+
-                "<td>"+element.Def+"</td>"+
-                "<td>"+element.SPD+"</td>"+
-                "</tr>";
-                $('#datatable').html(x);
-            }
-            else if(element.class.includes($('#input-text').val())){
-                console.log(element.id,element.name)
+            else if(element.name == word){
                 x +="<tr>"+
                 "<td>"+element.id+"</td>"+
                 "<td>"+element.name+"</td>"+
